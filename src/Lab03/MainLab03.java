46e0eb78cce94a69f310cfa5cab9fff6a349cdf0
Oleg Khrulov <hos@ukr.net>
2020-02-08 20:54:32 +0200

    The task is solved for the text type "String"

--- src/Lab03/MainLab03.java
+++ src/Lab03/MainLab03.java
@@ -13,12 +13,32 @@
     Необхідно обробити всі виключні  ситуації,  що  можуть  виникнути  під  час  виконання  програмного
     коду.  Всі  змінні  повинні  бути  описані  та  значення  їх  задані  у  виконавчому
     методі.
+    Для примера при вводе «oleggg khrvvvvlov”. Должно выдать "Oleg khrulov".
 */
 
 package Lab03;
 
+import java.util.Scanner;
+
 public class MainLab03 {
     public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Please enter a string:");
+            String string = scanner.nextLine();
+            System.out.println("\nIn each word, all previous occurrences of the last character of this word are deleted:");
+
+            for (String word : string.split(" ")) {
+                String lastCharacter = word.substring(word.length() - 1);
+                String result = word.replace(lastCharacter, "");
+                result = result + lastCharacter;
+                System.out.print(result + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("\n\nThe program is executed without errors");
+        }
     }
 }
